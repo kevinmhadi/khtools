2d5c745fb5694a073c975db02e76b22f3df4d7f0
kevinmhadi <kevinmhadi.gmail.com>
2025-02-13 08:17:12 -0500

    more namespace fixes

--- R/khtools-package.R
+++ R/khtools-package.R
@@ -1,9 +1,11 @@
-#' @importFrom S4Vectors with as.data.frame split mcols `mcols<-`
-#' @importFrom gUtils `%&%`
+#' khtools: R package to make life in R a bit easier.
+#' 
+#' @importFrom S4Vectors with as.data.frame split mcols mcols<-
+#' @importFrom gUtils %&%
 #' @importFrom methods setClass setGeneric setMethod setRefClass
 #' @importFrom GenomeInfoDb Seqinfo
-#' @importMethodsFrom GenomeInfoDb seqinfo `seqinfo<-` seqlevels `seqlevels<-` seqlengths `seqlengths<-` seqnames `seqnames<-`
-#' @importFrom data.table key `:=` rbindlist dcast.data.table
-#' @importFrom dplyr `%>%`
-#' @importFrom ggplot2 ggplot aes scale_fill_manual scale_colour_manual scale_color_manual geom_histogram geom_hex geom_point geom_path geom_bar geom_errorbar geom_smooth facet_wrap facet_grid xlab ylab scale_y_continuous scale_x_continuous theme theme_bw element_blank element_line element_text rel position_dodge
-NULL
+#' @importFrom data.table key := rbindlist dcast.data.table as.data.table setnames setkey setkeyv
+#' @importFrom dplyr %>%
+#' @importFrom ggplot2 ggplot aes scale_fill_manual scale_colour_manual scale_color_manual geom_histogram geom_hex geom_point geom_path geom_bar geom_errorbar geom_smooth facet_wrap facet_grid xlab ylab scale_y_continuous scale_x_continuous theme theme_bw element_blank element_line element_text rel position_dodge theme
+#' @importMethodsFrom GenomeInfoDb seqinfo seqinfo<- seqlevels seqlevels<- seqlengths seqlengths<- seqnames seqnames<-
+"_PACKAGE"
